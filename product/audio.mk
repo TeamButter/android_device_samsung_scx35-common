cd57e232bde689c208101830961ae0a1242bdc01
Nikit Sharma <nikitsharma92@gmail.com>
2017-10-22 11:59:59 +0530

    scx35-common: Build audio HAL
    Change-Id: Ia610fc1b7846a6900eb900f6df1937c3deb2845d

--- product/audio.mk
+++ product/audio.mk
@@ -1,5 +1,7 @@
 # Audio
 PRODUCT_PACKAGES += \
+        android.hardware.audio@2.0-impl \
+        android.hardware.audio.effect@2.0-impl \ 
 	audio_hw.xml \
 	audio_para \
 	audio_effects_vendor.conf \
