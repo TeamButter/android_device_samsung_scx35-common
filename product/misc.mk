f060044067071858bbb52edd6d4f5a1887043295
Nikit Sharma <nikitsharma92@gmail.com>
2018-02-21 01:07:06 +0530

    scx35-common: Fix derp :3 * Error due an additional space
    Change-Id: I10761b5cdf8fb6be2a23d951a9c3da624cdf4b6b

--- product/misc.mk
+++ product/misc.mk
@@ -1,6 +1,6 @@
 # Lights
 PRODUCT_PACKAGES += \
-        android.hardware.light@2.0-impl \ 
+        android.hardware.light@2.0-impl \
 	lights.sc8830
 
 # PowerHAL
--- product/wifi.mk
+++ product/wifi.mk
@@ -2,5 +2,5 @@
 PRODUCT_PACKAGES += \
         android.hardware.wifi@1.0-service \
         wificond \
-        wifilogd \ 
+        wifilogd \
 	macloader
