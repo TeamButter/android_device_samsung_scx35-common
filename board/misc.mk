d304032ac3228f872294dfca90b9987e34af9381
Nikit Sharma <nikitsharma92@gmail.com>
2018-02-22 04:12:19 +0530

    scx35-common: shim: Transition to TARGET_LD_SHIM_LIBS
    Change-Id: I620161e4fbc4b309defbe97ee7fa31e3e2294155

--- board/misc.mk
+++ board/misc.mk
@@ -4,6 +4,10 @@ EXTENDED_FONT_FOOTPRINT := true
 # Bionic
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
+# Shims
+TARGET_LD_SHIM_LIBS := \
+    /system/bin/gpsd|libgps_shim.so    
+
 # PowerHAL
 TARGET_POWERHAL_VARIANT := scx35
 
