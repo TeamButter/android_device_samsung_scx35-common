5bcfe9d2c20133b920bac50483cc1d263edee774
Nikit Sharma <nikitsharma92@gmail.com>
2018-02-22 20:16:49 +0530

    scx35-common: Add camera shim * From here https://github.com/TeamButter/android_device_samsung_core33g/commit/b15b2af57e2f8647055b55a85c5d05a5c1a3fdbd
    Change-Id: I5c66ab062421c7882b59e5f6fede983839ebff3a

--- board/misc.mk
+++ board/misc.mk
@@ -6,7 +6,8 @@ TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/bin/gpsd|libgps_shim.so    
+    /system/bin/gpsd|libgps_shim.so \
+    /system/lib/hw/camera.sc8830.so|/system/vendor/lib/libmemoryheapion.so
 
 # PowerHAL
 TARGET_POWERHAL_VARIANT := scx35
